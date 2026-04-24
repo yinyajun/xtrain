0fd30b104331afcb84e0de03a10091524d87660b
yinyajun <skyblueice234@gmail.com>
2026-04-25 00:56:43 +0800

    feat:init

--- sft_study/runs/e1_no_robots_full.sh
+++ sft_study/runs/e1_no_robots_full.sh
@@ -22,7 +22,7 @@ CMD=(
   --run_name "e1_no_robots_full" \
   --max_train_samples 9500 \
   --max_eval_samples 500 \
-  --num_train_epochs 2 \
+  --num_train_epochs 1 \
   --per_device_train_batch_size 2 \
   --per_device_eval_batch_size 4 \
   --gradient_accumulation_steps 16 \
@@ -36,10 +36,10 @@ CMD=(
   --warmup_ratio 0.03 \
   --weight_decay 0.01 \
   --quantization auto \
-  --lora_r 128 \
-  --lora_alpha 256 \
+  --lora_r 64 \
+  --lora_alpha 128 \
   --lora_dropout 0.05 \
-  --target_modules q_proj k_proj v_proj o_proj up_proj down_proj gate_proj \
+  --target_modules q_proj k_proj v_proj o_proj up_proj down_proj gate_proj lm_head\
   --report_to "$REPORT_TO" \
   --gradient_checkpointing \
   --seed 42
